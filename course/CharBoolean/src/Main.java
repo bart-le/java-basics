dac2a65c6f37ea24b71b5f41fef5bdcc34a9cfb8
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 20:03:32 +0200

    Char and boolean

---
+++ course/CharBoolean/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		char myChar = 'C';
+		char myUnicodeChar = '\u0043';
+		System.out.println(myChar);
+		System.out.println(myUnicodeChar);
+
+		char myCopyrightChar = '\u00A9';
+		System.out.println(myCopyrightChar);
+
+		boolean myTrueBoolean = true;
+		boolean myFalseBoolean = false;
+
+		boolean isUnderaged = false;
+	}
+}
