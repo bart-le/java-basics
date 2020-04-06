1513d285026df2be27217658c82ad3523cc3e319
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 19:59:47 +0200

    Floating point number

---
+++ challenges/FloatDouble/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		double kgInLbs = 0.45359237;
+		int lbs = 420;
+		double kg = lbs * kgInLbs;
+
+		System.out.println(lbs + " pounds is equal to " + kg + " kilogrammes.");
+	}
+}
