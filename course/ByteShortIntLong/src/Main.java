f179fe37280117846e3d66041104059ccd4bdb6c
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 17:12:29 +0200

    Byte and short

--- course/ByteShortIntLong/src/Main.java
+++ course/ByteShortIntLong/src/Main.java
@@ -2,6 +2,16 @@ public class Main {
 	public static void main(String[] args) {
 		int myValue = 10000;
 
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("Byte minimum value: " + myMinByteValue);
+		System.out.println("Byte maximum value: " + myMaxByteValue);
+
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("Short minimum value: " + myMinShortValue);
+		System.out.println("Short maximum value: " + myMaxShortValue);
+
 		int myMinIntValue = Integer.MIN_VALUE;
 		int myMaxIntValue = Integer.MAX_VALUE;
 		System.out.println("Integer minimum value: " + myMinIntValue);
