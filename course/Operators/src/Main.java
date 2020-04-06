009f2ee903312ee79911ec4204e230fc31e044c6
Bart Le <le.bartlomiej@gmail.com>
2020-04-06 20:13:29 +0200

    Logical AND

--- course/Operators/src/Main.java
+++ course/Operators/src/Main.java
@@ -41,5 +41,15 @@ public class Main {
 			System.out.println("Hold in isolation.");
 			System.out.println("Give medication.");
 		}
+
+		int topScore = 80;
+		if (topScore < 100) {
+			System.out.println("You got the high score!");
+		}
+
+		int secondTopScore = 81;
+		if (topScore > secondTopScore && topScore < 100) {
+			System.out.println("Greater than second top score and less than 100.");
+		}
 	}
 }
