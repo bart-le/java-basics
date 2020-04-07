6f6d0b4a34e43191b7e60798f5b7e4b65ccbb471
Bart Le <le.bartlomiej@gmail.com>
2020-04-07 16:04:58 +0200

    Even number

---
+++ challenges/EvenNumber/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+	public static void main(String[] args) {
+		int number = 4;
+		int finishNumber = 20;
+		int count = 0;
+
+		while (number <= finishNumber) {
+			number++;
+			if (!isEvenNumber(number)) continue;
+			System.out.println("Even number: " + number);
+			count++;
+			if (count == 5) break;
+		}
+	}
+
+	public static boolean isEvenNumber(int number) {
+		return number % 2 == 0;
+	}
+}
