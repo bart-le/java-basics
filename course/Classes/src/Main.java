62fc101eaa19a47a1d2b4a41f07cfbeadbea4544
Bart Le <le.bartlomiej@gmail.com>
2020-04-08 16:05:21 +0200

    Classes

---
+++ course/Classes/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+	public static void main(String[] args) {
+		Car toyota = new Car();
+		toyota.setModel("Corolla");
+		System.out.println("Model is " + toyota.getModel());
+	}
+}
