65d4a29886bdaad11b584ef69896a13ce1cc2c37
Bart Le <le.bartlomiej@gmail.com>
2020-04-09 16:19:59 +0200

    Inheritance

---
+++ challenges/Inheritance/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		Veyron bugatti = new Veyron(36);
+		bugatti.steer(45);
+		bugatti.accelerate(30);
+		bugatti.accelerate(20);
+		bugatti.accelerate(-40);
+	}
+}
