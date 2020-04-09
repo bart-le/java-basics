59cae64dc9ba663c08a669bb226545ebdf5f5efd
Bart Le <le.bartlomiej@gmail.com>
2020-04-09 20:32:06 +0200

    Encapsulation

---
+++ course/Encapsulation/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+	public static void main(String[] args) {
+		Player player = new Player("Bart", 50, "Lightsaber");
+		System.out.println("Initial health is " + player.getHealth());
+	}
+}
---
+++ course/Encapsulation/src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+	private String name;
+	private int hitPoints;
+	private String weapon;
+
+	public Player(String name, int health, String weapon) {
+		this.name = name;
+		this.hitPoints = health > 0 && health <= 100 ? health : 100;
+		this.weapon = weapon;
+	}
+
+	public void loseHealth(int damage) {
+		this.hitPoints = this.hitPoints - damage;
+		if (this.hitPoints <= 0) {
+			System.out.println("Player knocked out.");
+		}
+	}
+
+	public int getHealth() {
+		return hitPoints;
+	}
+}
