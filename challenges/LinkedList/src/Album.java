324c6398ce5149c628cb679d82f9f64c0473ac89
Bart Le <le.bartlomiej@gmail.com>
2020-04-13 23:27:45 +0200

    LinkedList

---
+++ challenges/LinkedList/src/Album.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+	private String name;
+	private String artist;
+	private ArrayList<Song> songs;
+
+	public Album(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+		this.songs = new ArrayList<>();
+	}
+
+	public boolean addSong(String title, double duration) {
+		if (findSong(title) == null) {
+			this.songs.add(new Song(title, duration));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private Song findSong(String title) {
+		for (Song song : this.songs) {
+			if (song.getTitle().equals(title))
+				return song;
+		}
+
+		return null;
+	}
+
+	public boolean addSongToPlaylist(int trackNumber, LinkedList<Song> playlist) {
+		int index = trackNumber - 1;
+		if ((index >= 0) && (index < this.songs.size())) {
+			playlist.add(this.songs.get(index));
+			return true;
+		} else {
+			System.out.println("This album does not have #" + trackNumber + " track");
+			return false;
+		}
+	}
+
+	public boolean addSongToPlaylist(String title, LinkedList<Song> playlist) {
+		Song song = findSong(title);
+		if (findSong(title) != null) {
+			playlist.add(song);
+			return true;
+		} else {
+			System.out.println("This album does not have a song titled " + title);
+			return false;
+		}
+	}
+}
