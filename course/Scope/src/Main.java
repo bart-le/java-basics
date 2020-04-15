7c3b331ed4bad21924bc8f5617c4d6062ac4b52a
Bart Le <le.bartlomiej@gmail.com>
2020-04-15 02:44:58 +0200

    Scope

---
+++ course/Scope/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+		String varFour = "This is private to main()";
+
+		ScopeCheck scopeInstance = new ScopeCheck();
+		scopeInstance.useInner();
+		System.out.println("scopeInstance varOne: " + scopeInstance.getVarOne());
+		System.out.println(varFour);
+		scopeInstance.timesTwo();
+
+		System.out.println("------");
+
+		ScopeCheck.InnerClass innerClass = scopeInstance.new InnerClass();
+		innerClass.timesTwo();
+	}
+}
---
+++ course/Scope/src/ScopeCheck.java
@@ -0,0 +1,41 @@
+public class ScopeCheck {
+	public int publicVar = 0;
+	private int varOne = 1;
+
+	public ScopeCheck() {
+		System.out.println("ScopeCheck created");
+		System.out.println("publicVar: " + publicVar);
+		System.out.println("varOne: " + varOne);
+	}
+
+	public int getVarOne() {
+		return varOne;
+	}
+
+	public void timesTwo() {
+		int varTwo = 2;
+		for (int i = 0; i <= 10; i++)
+			System.out.println(i + " * 2 = " + (i * varTwo));
+	}
+
+	public void useInner() {
+		InnerClass innerClass = new InnerClass();
+		System.out.println("varThree from outer class: " + innerClass.varThree);
+	}
+
+	public class InnerClass {
+		private int varThree = 3;
+
+		public InnerClass() {
+			System.out.println("InnerClass created");
+			System.out.println("varOne: " + varOne);
+			System.out.println("varThree: " + varThree);
+		}
+
+		public void timesTwo() {
+			System.out.println("varOne is still available: " + varOne);
+			for (int i = 0; i <= 10; i++)
+				System.out.println(i + " * 2 = " + (i * varThree));
+		}
+	}
+}
